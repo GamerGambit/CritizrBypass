510aac7ae1d00d38879019c74b39affe18128e2a
Tyler Duffus <tyled94@hotmail.com>
2023-08-30 00:01:37 +1000

    Migrated to plain Javascript from JQuery

--- workaround.user.js
+++ workaround.user.js
@@ -9,6 +9,13 @@
 // @grant        none
 // ==/UserScript==
 
+const FeedbackType = {
+    Issue: 0,
+    Compliment: 1,
+    Question: 2,
+    Suggestion: 3
+};
+
 let storeids = [];
 const negativeWords = [
     "unhappy", "not happy", "terrible", "cold", "paid", "asked", "extra", "missing", "forgot", "doubt", "poor", "dry", "burnt", "undercooked", "soggy", "lacking", "misleading", "wrong", "pathetic", "dissapoint", "incorrect", "allergic",
@@ -26,19 +33,46 @@ function putAPIKey(key)
     localStorage.setItem("apikey", key);
 }
 
-function getFromID()
+async function getFromID()
 {
     const id = location.pathname.split("/")[4];
-    return $.ajax({
-        url: "https://critizr.com/bo/api/v2/threads/" + id,
-        dataType: "json",
-        async: false
-    });
+    const response = await fetch("https://critizr.com/bo/api/v2/threads/" + id);
+    return await response.json();
 }
 
 function shouldProcess(json)
 {
     return storeids.includes(parseInt(json.place.external_id, 10));
+}
+
+async function confirmAuth()
+{
+    let apikey = getAPIKey();
+    while (apikey === null || apikey.length == 0)
+    {
+        apikey = prompt("Enter your API key:");
+
+        // Cancel clicked, stop pestering.
+        if (apikey === null)
+            return;
+    }
+
+    // Make sure there is at least 1 store registered to this key.
+    // The server will return an empty array even for bad keys
+    const response = await fetch("https://critizr.gambyy.xyz/AuthorizedKeys/Stores/" + apikey);
+    const json = await response.json();
+
+    // No stores assigned to this key
+    if (!response.ok || json.length == 0)
+    {
+        alert("Invalid API Key");
+        confirmAuth();
+        return;
+    }
+
+    // We have stores, so assign them to `storeids`
+    storeids = json;
+    putAPIKey(apikey);
 }
 
 function isPotentiallyNegativeMessage(json)
@@ -63,10 +97,10 @@ function delay(ms) {
 
 function speedyMarkDone()
 {
-    $("[data-trigger-action='mark_as_done']").trigger("click");
+    document.querySelector("[data-trigger-action='mark_as_done']").click();
 }
 
-async function processDissatisfactionAlert(json, textStatus, xhr)
+async function processDissatisfactionAlert(json)
 {
     var name = json.last_item.object.user.first_name.trim();
     const age = Math.round((Date.now() - Date.parse(json.last_item.object.created_at)) / 1000); // how old this dissatisfaction is in seconds
@@ -81,7 +115,7 @@ async function processDissatisfactionAlert(json, textStatus, xhr)
 
     if (!shouldProcess(json))
     {
-        console.log("Ignoring. " + json.place.external_id + " not in " + storeids.join(", "));
+        console.log(json.id + " | Ignoring. " + json.place.external_id + " not in " + storeids.join(", "));
         return;
     }
 
@@ -103,45 +137,45 @@ async function processDissatisfactionAlert(json, textStatus, xhr)
 
     // Feedback needs a "type" (issue, compliment, suggestion, question)
     // For dissatisfaction, always select "issue"
-    if ($(".btn-howto").length > 0)
+    if (document.querySelector(".btn-howto"))
     {
         console.log(json.id + " | Setting feedback type to \"Issue\"");
-        $(".type-chooser .btn-secondary:first-child").trigger("click"); // Click "Issue" type button
+        document.querySelector(".type-chooser .btn-secondary:first-child").click(); // Click "Issue" type button
         await delay(5000);
     }
 
-    console.log(json.id + " | Clicking Reply", $("[data-trigger-action='reply']"));
-    $("[data-trigger-action='reply']").trigger("click"); // Click "Reply"
+    console.log(json.id + " | Clicking Reply");
+    document.querySelector("[data-trigger-action='reply']").click(); // Click "Reply"
 
     // We cant fill out the reply immediately after clicking "Reply" so wait.
     await delay(5000);
 
     console.log(json.id + " | Filling out reply field");
     // Fill out the reply message
-    $("#reply-pane-view-textarea")
-        .val("Hi" + name + ", thanks for your feedback. One of the management team will review this in the next 24-48 hours. Thank you for your patience.")
-        .trigger("input");
+    const textarea = document.querySelector("#reply-pane-view-textarea");
+    textarea.value = "Hi" + name + ", thanks for your feedback. One of the management team will review this in the next 24-48 hours. Thank you for your patience.";
+    textarea.dispatchEvent(new Event("input", { bubbles: true }));
 
     console.log(json.id + " | Toggling hold switch");
-    $(".toggle-switch-thumb").trigger("click"); // Set the reply mode to put the customer on hold
-    $(".send-button > button:first-child").trigger("click"); // Click "Send and put on hold" button.
+    document.querySelector(".toggle-switch-thumb").click(); // Set the reply mode to put the customer on hold
+    document.querySelector(".send-button > button:first-child").click(); // Click "Send and put on hold" button.
 
     // Potentially wait for the spell checker modal
     await delay(5000);
 
     // Spell checker modal
-    if ($(".spell-checker").length > 0)
+    if (document.querySelector(".spell-checker"))
     {
         console.log(json.id + " | Detected spell checker modal");
         // Click "Confirm and Send"
-        $(".modal-footer button:last-child").trigger("click");
+        document.querySelector(".modal-footer button:last-child").click();
 
         // Wait for the modal to disappear
         await delay(5000);
     }
 }
 
-async function processMessage(json, textStatus, xhr)
+async function processMessage(json)
 {
     // Messages not marked as "need_reply" can be ignored.. probably.
     if (json.state != "need_reply")
@@ -168,17 +202,18 @@ async function processMessage(json, textStatus, xhr)
     if (isPotentiallyNegativeMessage(json))
     {
         console.log(json.id + " | Potentially negative message, putting on hold.");
-        $("[data-trigger-action='mark_as_active']").click()
+        document.querySelector("[data-trigger-action='mark_as_active']").click()
     }
     else
     {
         if (json.last_item.object.hasOwnProperty("survey_participation") && json.last_item.object.survey_participation.answer_to_highlight.value > 8)
         {
             console.log(json.id + " | NPS > 8, marking as compliment");
-            $(".type-chooser .btn-secondary").eq(1).click(); // Click "Compliment" type button
+            document.querySelectorAll(".type-chooser .btn-secondary")[FeedbackType.Compliment].click(); // Click "Compliment" type button
             await delay(5000); // Wait for the page to rehydrate after choosing feedback type
         }
 
+        console.log(json.id + " | Marking as done");
         speedyMarkDone();
     }
 
@@ -188,80 +223,50 @@ async function processMessage(json, textStatus, xhr)
 
 async function main()
 {
-    const alerts = $("div.alert-navs-region > div > nav").children();
-    const messages = $("div.need-or-should-reply-navs-region > div > nav").children();
+    const alerts = document.querySelector("div.alert-navs-region > div > nav").children;
+    const messages = document.querySelector("div.need-or-should-reply-navs-region > div > nav").children;
 
     // Dissatisfaction Alerts
     console.log("Checking Dissatisfaction Alerts");
-    for (let i = 0; i < alerts.length; ++i)
+    for (const alert of alerts)
     {
-        alerts.eq(i).trigger("click"); // Click the element
+        alert.click(); // Click the element
 
         // Wait until the page has rehydrated.
         await delay(5000);
 
-        const result = getFromID();
-        await processDissatisfactionAlert(result.responseJSON, result.statusText, result);
+        const result = await getFromID();
+        await processDissatisfactionAlert(result);
     };
 
     // Messages (could be positive feedback or replies afaik)
-    for (let i = 0; i < messages.length; ++i)
+    for (const message of messages)
     {
-        messages.eq(i).trigger("click"); // Click the element
+        message.click(); // Click the element
 
         // Wait until the page has rehydrated.
         await delay(5000);
 
-        const result = getFromID();
-        await processMessage(result.responseJSON, result.statusText, result);
+        const result = await getFromID();
+        await processMessage(result);
     };
 
     // reload the page every 20 minutes to keep the sessions active
-    setTimeout(function() {
-        console.log("Refreshing page");
-        window.location.replace("https://critizr.com/pro/messages/active/");
-    }, 1000 * 60 * 20);
+    await delay(1000 * 60 * 20);
+    console.log("Refreshing page");
+    window.location.replace("https://critizr.com/pro/messages/active/");
 }
 
-(function() {
+(async function() {
     'use strict';
 
     console.log("Loaded Page, checking auth");
 
-    let apikey = getAPIKey();
-    do
-    {
-        // No API key found in query parameters or localstorage so prompt for one
-        if (!apikey || apikey.length == 0)
-        {
-            apikey = prompt("Enter your API key:");
-        }
-
-        // Cancel clicked, stop pestering.
-        if (apikey === null)
-            return;
-
-        // Make sure there is at least 1 store registered to this key.
-        // The server will return an empty array even for bad keys
-        const response = $.ajax({url: "https://critizr.gambyy.xyz/AuthorizedKeys/Stores/" + apikey, type: "GET", dataType: "json", async: false});
-
-        // No stores assigned to this key
-        if (!response.responseJSON || response.responseJSON.length == 0)
-        {
-            alert("Invalid API Key");
-            apikey = null;
-            continue;
-        }
-
-        // We have stores, so assign them to `storeids`
-        storeids = response.responseJSON;
-        putAPIKey(apikey);
-        break;
-    }
-    while(true);
+    await confirmAuth();
 
     console.log("Automating feedback for store ids: " + storeids.join(", "));
 
     // Check everything after 5 seconds.. hopefully this is enough time for critizr to load everything behind the scenes
-    setTimeout(main, 5000);
+    await delay(5000);
+    await main();
 })();

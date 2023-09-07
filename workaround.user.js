7f9424e482227f2cf487a936e7bd3d5a5fc9d1e2
Tyler Duffus <tyled94@hotmail.com>
2023-09-07 21:14:24 +1000

    Added more negative keywords

--- workaround.user.js
+++ workaround.user.js
@@ -21,7 +21,7 @@ const FeedbackId = {
 let storeids = [];
 const negativeWords = [
     "unhappy", "not happy", "terrible", "cold", "paid", "asked", "extra", "missing", "forgot", "doubt", "poor", "dry", "burnt", "undercooked", "soggy", "lacking", "misleading", "wrong", "pathetic", "dissapoint", "incorrect", "allergic",
-    "upset", "cut", "late", "barely", "but", "ordered"
+    "upset", "cut", "late", "barely", "but", "ordered", "mistake", "dissatisfied"
 ];
 const dissatisfactionReply = "Hi @NAME@, thanks for your feedback. One of the management team will review this in the next 24-48 hours. Thank you for your patience.";
 const promoterReply = "Hi @NAME@, we genuinely appreciate your positive feedback and will pass this on to the rest of the team.\n\nKind regards,\n@STORE@";
